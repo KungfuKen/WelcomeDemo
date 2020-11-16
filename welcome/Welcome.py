ba7b1d5efae884380a779ca35b625e8a028c7d87
Dalitso <tenthani2@gmail.com>
2020-11-16 15:39:20 +0200

    pypi changes

--- welcome/Welcome.py
+++ welcome/Welcome.py
@@ -1,12 +1,31 @@
 import os 
+
 class Welcome:
+    """
+    This class writes a string a user inputs into a file on a computer in one function and writes strings in file in another function,
+    2 function contained:
+            1. Configure() - inputs string in file
+            2. Print_welcome_message() - outputs strings in file
+    """
     def __init__(self):
+        """ 
+        Empty constructor
+        """
         pass
 
     def Configure(self, filename):
+        """
+        The file to be written on is taken as an argument. Using a for loop the file is searched for in path of the computer
+        The path searched is the current working director
+        Paramerter:
+                filename - file to be added string to
+        Outputs:
+                "File found" or "File not found" 
+        """
         word = input("Enter a sentence: ")
         fail = True
-        for root, dirs, files in os.walk("C:\\Users\\tenth\\OneDrive\\Desktop\\"):
+        path = os.getcwd()
+        for root, dirs, files in os.walk(path):
             if filename in files:
                 print("File Found")
                 file1 = open(filename, "a")
@@ -19,8 +38,17 @@ class Welcome:
             print("File Not Found")
 
     def Print_welcome_message(self, filename):
+        """
+        The file to be read on is taken as an argument. Using a for loop the file is searched for in path of the computer
+        The path searched is the current working director
+        Paramerter:
+                filename - file to be read
+        Outputs:
+                Content of the file or "File not found"
+        """
         fail = True
-        for root, dirs, files in os.walk("C:\\Users\\tenth\\OneDrive\\Desktop\\"):
+        path = os.getcwd()
+        for root, dirs, files in os.walk(path):
             if filename in files:
                 print("File Found")
                 file1 = open(filename, "r")
@@ -31,7 +59,5 @@ class Welcome:
         if fail:
             print("File Not Found")
 
-newWel = Welcome()
-newWel.Configure("test.txt")
-newWel.Print_welcome_message("test.txt")
+
 
